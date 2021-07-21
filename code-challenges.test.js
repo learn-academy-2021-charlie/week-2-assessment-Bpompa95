613e838d365a4f3a4a53b6be27d6ffcdfacbf819
Bpompa95 <brianpompa02@gmail.com>
2021-07-20 23:25:15 -0700

    i did my best with the last two questions

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -1,6 +1,6 @@
 // ASSESSMENT 2: Coding practical questions with Jest
 
-const { expect } = require("@jest/globals")
+const { expect, describe, test } = require("@jest/globals")
 
 // const { test } = require("jest-circus")
 // const { describe } = require("yargs")
@@ -30,16 +30,16 @@ const { expect } = require("@jest/globals")
 //         var num1 =15
 //         var num2 = 0
 //         var num3 = -7
-//         expect(divideBy(num1)).toEqual(`15 is not divisble by three`)
+//         expect(divideBy(num1)).toEqual(`15 is divisble by three`)
 //         expect(divideBy(num2)).toEqual(`0 is divisble by three`)
 //         expect(divideBy(num3)).toEqual(`-7 is not divisble by three`)
 
 //        })
 //     })
 //     const divideBy = (num) => {
-//          if(num / 3 === 0){
+//          if(num % 3 === 0){
 //           return `${num} is divisble by three`
-//           }else if(num / 3){
+//           }else if(num % 3){
 //               return `${num} is not divisble by three`
 //           }
 //         }
@@ -65,39 +65,56 @@ const { expect } = require("@jest/globals")
 // --------------------2) Create a function that takes in an array of words and returns an array with all the words capitalized.
 
 // a) Create a test with expect statements for each of the variables provided.
+describe("capitalizeFirst", () => {
+    test("returns an array with all the words capitalized", () => {
+        var randomNouns1 = ["streetlamp", "potato", "teeth", "conclusion", "nephew"]
+        var randomNouns2 = ["temperature", "database", "chopsticks", "mango", "deduction"]
+        expect(capitalizeFirst(randomNouns1)).toEqual("streetlamp", "potato", "teeth", "conclusion", "nephew")
+        expect(capitalizeFirst(randomNouns2)).toEqual("temperature", "database", "chopstick", "mango", "deduction")
+    })
+})
 
-var randomNouns1 = ["streetlamp", "potato", "teeth", "conclusion", "nephew"]
+
+// var randomNouns1 = ["streetlamp", "potato", "teeth", "conclusion", "nephew"]
 // Expected output: ["Streetlamp", "Potato", "Teeth", "Conclusion", "Nephew"]
 
-var randomNouns2 = ["temperature", "database", "chopsticks", "mango", "deduction"]
+// var randomNouns2 = ["temperature", "database", "chopsticks", "mango", "deduction"]
 // Expected output: ["Temperature", "Database", "Chopsticks", "Mango", "Deduction"]
 
 
 
 // b) Create the function that makes the test pass.
+const capitalizeFirst = (array) => {
+for(var i = 0; i < array.length; i++){
+    array[i] = array[i].toUpperCase(i);
+}
+}
 
-// var capitalizeFirst = function(array) {
-//     if (array.length === 0) return [];
-  
-//   let randomNouns1 = array[0].split('');
-//   randomNouns1[0] = randomNouns1[0].toUpperCase();
-  
-//   return [randomNouns1.join('')].concat(capitalizeFirst(array.slice(1)));
-  
-// };
-
+// i feel like i am almost there for question 2 but i don't know what else i was missing i even try console.log and it wouldn't work hahaha but it's all good i try my best for this one and it was a hell of a battle.
 
 // --------------------3) Create a function that takes in a string and logs the index of the first vowel.
 
 // a) Create a test with expect statements for each of the variables provided.
 
-var vowelTester1 = "learn"
+describe("firstVowel", () => {
+    test("logs the index of the first vowel", () => {
+        var vowelTester1 = "learn"
+        var vowelTester2 = "academy"
+        var vowelTester3 = "challenge"
+        expect(firstVowel(vowelTester1)).toEqual(1)
+        expect(firstVowel(vowelTester2)).toEqual(0)
+        expect(firstVowel(vowelTester3)).toEqual(2)
+    })
+})
+// var vowelTester1 = "learn"
 // Expected output: 1
-var vowelTester2 = "academy"
+// var vowelTester2 = "academy"
 // Expected output: 0
-var vowelTester3 = "challenge"
+// var vowelTester3 = "challenge"
 // Expected output: 2
 
 
 
 // b) Create the function that makes the test pass.
+
+// for question 3 it was a tricky one i couldn't figure out a function to do this.
